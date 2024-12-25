0276e773ef65294640d5e2f7e72efa652bb216b2
mayankvagadiya2023 <mayankvagadia17@gmail.com>
2024-12-25 18:32:51 +0530

    First API

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
