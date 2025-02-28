0e2bb38c8f24c60ee4ca4a6626f6e1ecee40c4ca
mayankvagadiya2023 <mayankvagadia17@gmail.com>
2025-02-28 12:07:49 +0530

    changes

--- backend/routes/category.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAllCategory } = require("../controllers/category");
-
-router.route("/getAllCategory").get(getAllCategory);
-
-module.exports = router;
--- backend/routes/wishlist.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { addwishlist } = require("../controllers/wishlist");
-
-router.route("/addwishlist").post(addwishlist);
-
-module.exports = router;
