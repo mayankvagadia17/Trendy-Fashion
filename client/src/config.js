42578a1a82880ceb1a61902e367fa5201cc1877f
mayankvagadiya2023 <mayankvagadia17@gmail.com>
2025-03-12 17:15:58 +0530

    baseUrl change

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // For Production
-// export const BASE_URL = "https://trendy-fashion.onrender.com";
+export const BASE_URL = "https://trendy-fashion.onrender.com";
 
 // For Local
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
